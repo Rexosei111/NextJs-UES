679d3480a780f20e047270b0105c45489ddfcf4c
Kyei Samuel <kyeisamuel931@gmail.com>
2022-06-02 14:21:30 +0000

    slider done

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,46 +9,72 @@ export default function HeroSection() {
   return (
     <Container
       maxWidth="xl"
+      disableGutters
       sx={{
         height: "calc(100vh - 64px - 50px)",
-        backgroundColor: "rgba(0,0,0,0.4)",
-        backgroundImage: "url('/images/ball2.jpg')",
-        backgroundPosition: "center bottom",
-        backgroundBlendMode: "overlay",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
         width: "100%",
-        display: "flex",
-        alignItems: "center",
+        position: "relative",
       }}
     >
-      <Container maxWidth="md">
-        <Typography variant="h2" color="text.secondary">
-          Welcome,
-        </Typography>
-        <Typography color="text.primary" variant="h4">
-          To UMaT Ethihad Sports PlayGround
-          <span role="img" aria-label="ball" style={{ fontSize: "30px" }}>
-            ⚽
-          </span>
-        </Typography>
-        <Typography variant="h6" color={"text.secondary"}>
-          Book our playground for your sporting activities
-        </Typography>
-        <ButtonGroup variant="contained" sx={{ mt: 2 }}>
-          <Button
-            sx={{ color: "white" }}
-            onClick={() => {
-              setBackdropComponent("form");
-              setOpen(true);
-            }}
-          >
-            Book Now
-          </Button>
-          <Button variant="outlined" color="secondary" sx={{ ml: 2 }}>
-            Terms and Conditions
-          </Button>
-        </ButtonGroup>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, .6)",
+        }}
+      ></Box>
+      <video
+        autoPlay
+        muted
+        loop
+        style={{ height: "100%", width: "100%", objectFit: "cover" }}
+      >
+        <source src="/images/videoplayback.mp4" type="video/mp4" />
+      </video>
+      <Container
+        maxWidth="md"
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          widht: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box>
+          <Typography variant="h2" color="text.secondary">
+            Welcome,
+          </Typography>
+          <Typography color="text.primary" variant="h4">
+            To UMaT Ethihad Sports PlayGround
+            <span role="img" aria-label="ball" style={{ fontSize: "30px" }}>
+              ⚽
+            </span>
+          </Typography>
+          <Typography variant="h6" color={"text.secondary"}>
+            Book our playground for your sporting activities
+          </Typography>
+          <ButtonGroup variant="contained" sx={{ mt: 2 }}>
+            <Button
+              sx={{ color: "white" }}
+              onClick={() => {
+                setBackdropComponent("form");
+                setOpen(true);
+              }}
+            >
+              Book Now
+            </Button>
+            <Button variant="outlined" color="secondary" sx={{ ml: 2 }}>
+              Terms and Conditions
+            </Button>
+          </ButtonGroup>
+        </Box>
       </Container>
     </Container>
   );
